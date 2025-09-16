919cb90cd6ce71e2ff89ecdeaf1cb652ef6a1c20
D3lta-Q <zackry.hujber02@gmail.com>
2025-09-16 05:54:50 -0600

    Update grass_placer.py

--- operators/grass_placer.py
+++ operators/grass_placer.py
@@ -9,7 +9,7 @@ import random
 from mathutils import Vector, Matrix
 from mathutils.bvhtree import BVHTree
 from math import radians, sqrt
-import numpy as np
+# import numpy as np
 
 # Property Groups
 class DAYZ_TargetObject(bpy.types.PropertyGroup):
@@ -452,116 +452,6 @@ class DAYZ_OT_GenerateGrass(bpy.types.Operator):
             clean_name = clean_name.replace(suffix, '')
         
         return clean_name
-
-# UI Panel for grass placer
-class DAYZ_PT_GrassPlacerPanel(bpy.types.Panel):
-    """Grass placer panel within DayZ tools"""
-    bl_label = "Grass Placer"
-    bl_idname = "DAYZ_PT_grass_placer_panel"
-    bl_space_type = 'VIEW_3D'
-    bl_region_type = 'UI'
-    bl_category = 'DayZ Tools'
-    bl_parent_id = "DAYZ_PT_main_panel"
-
-    def draw(self, context):
-        layout = self.layout
-        settings = context.scene.dayz_grass_placer_settings
-
-        # Target Objects Section
-        box = layout.box()
-        box.label(text="Target Objects", icon='SURFACE_DATA')
-        box.operator("dayz.set_grass_targets", text="Set Selected as Targets", icon='PLUS')
-        
-        if settings.target_objects:
-            col = box.column()
-            for target in settings.target_objects:
-                if target.obj:
-                    col.label(text=f"• {target.obj.name}", icon='OBJECT_DATA')
-                else:
-                    col.label(text="• (Missing Object)", icon='ERROR')
-        else:
-            box.label(text="No targets set", icon='INFO')
-
-        layout.separator()
-
-        # Grass Objects Section
-        box = layout.box()
-        box.label(text="Grass Objects", icon='MESH_DATA')
-        
-        row = box.row()
-        row.operator("dayz.add_grass_object", text="Add", icon='ADD')
-        row.operator("dayz.remove_grass_object", text="Remove", icon='REMOVE')
-        
-        # Show grass objects with proper object selectors
-        if settings.grass_objects:
-            for i, grass_item in enumerate(settings.grass_objects):
-                item_box = box.box()
-                row = item_box.row()
-                
-                # Object selector with eyedropper
-                row.prop(grass_item, "obj", text=f"Grass {i+1}")
-                
-                # Weight slider
-                weight_row = item_box.row()
-                weight_row.prop(grass_item, "weight", text="Weight", slider=True)
-        else:
-            box.label(text="No grass objects added", icon='INFO')
-
-        layout.separator()
-
-        # Placement Settings
-        box = layout.box()
-        box.label(text="Placement Settings", icon='SETTINGS')
-        
-        col = box.column()
-        col.prop(settings, "total_count")
-        col.prop(settings, "surface_offset")
-        col.prop(settings, "distribution_seed")
-
-        layout.separator()
-
-        # Distribution Settings
-        box = layout.box()
-        box.label(text="Distribution", icon='FORCE_TURBULENCE')
-        
-        col = box.column()
-        col.prop(settings, "clumping_factor", slider=True)
-
-        layout.separator()
-
-        # Variation Settings
-        box = layout.box()
-        box.label(text="Variation", icon='MODIFIER_DATA')
-        
-        col = box.column()
-        row = col.row(align=True)
-        row.prop(settings, "scale_min")
-        row.prop(settings, "scale_max")
-        col.prop(settings, "random_rotation")
-
-        layout.separator()
-
-        # Organization
-        box = layout.box()
-        box.label(text="Organization", icon='OUTLINER')
-        
-        col = box.column()
-        col.prop(settings, "parent_to_empty")
-        
-        col.separator()
-        col.label(text="Merge Options:")
-        col.prop(settings, "merge_all_grass")
-        col.prop(settings, "merge_by_variant")
-        
-        if settings.merge_all_grass:
-            col.label(text="Significantly increases processing time", icon='ERROR')
-
-        layout.separator()
-
-        # Generate Button
-        row = layout.row()
-        row.scale_y = 2.0
-        row.operator("dayz.generate_grass", text="Generate Grass", icon='MOD_PARTICLES')
 
 # Classes to register
 grass_classes = (
